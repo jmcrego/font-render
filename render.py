50b38be06de2e821a2c4b68f5c36f269e8257000
Josep Crego <jmcrego@gmail.com>
2025-10-10 10:50:05 +0200

    refactor

--- render.py
+++ render.py
@@ -4,7 +4,6 @@ class GridGlyphRenderer:
     def __init__(self, font_path="fonts/unifont-16.0.04.ttf", cell_size=16, max_cols=16, img_mode="L"):
         """
         Render tokens into a single-row sequence of characters.
-
         Args:
             font_path: Path to monospaced TTF font.
             cell_size: Size of each character cell (square).
@@ -15,12 +14,9 @@ class GridGlyphRenderer:
         self.cell_size = cell_size
         self.max_cols = max_cols
         self.img_mode = img_mode
-
         # Load font
         self.font = ImageFont.truetype(font_path, cell_size)
         self.char_cache = {}
-        
-
 
     def render_char(self, ch):
         """Render a single character into a cell_size x cell_size image with caching."""
@@ -40,12 +36,10 @@ class GridGlyphRenderer:
         """
         token = token[:self.max_cols]
         num_chars = len(token)
-
         # Create single-row canvas
         width = self.cell_size * num_chars
         height = self.cell_size
         img = Image.new(self.img_mode, (width, height), 0)
-
         # Paste each character sequentially
         for i, ch in enumerate(token):
             char_img = self.render_char(ch)
@@ -53,35 +47,16 @@ class GridGlyphRenderer:
 
         return img
 
-
-    def render_token2(self, token):
-        """
-        Compose up to n_rows x n_cols characters into a single image.
-        If token has fewer characters than max, empty cells are left black.
-        """
-        max_chars = self.n_rows * self.n_cols
-        token = token[:max_chars]
-
-        img = Image.new(self.img_mode, (self.cell_size*self.n_cols, self.cell_size*self.n_rows), 0)
-
-        for i, ch in enumerate(token):
-            row = i // self.n_cols
-            col = i % self.n_cols
-            char_img = self.render_char(ch)
-            img.paste(char_img, (col*self.cell_size, row*self.cell_size))
-
-        return img
-
     def __call__(self, tokens):
         return [self.render_token(t) for t in tokens]
 
+if __name__ == "__main__":
 
-font_path = "fonts/NotoSansMono-VariableFont_wdth,wght.ttf"
-font_path = "fonts/unifont-16.0.04.ttf"
-renderer = GridGlyphRenderer(font_path, cell_size=16, max_cols=16)
+    font_path = "fonts/unifont-16.0.04.ttf"
+    renderer = GridGlyphRenderer(font_path, cell_size=16, max_cols=16)
 
-img = renderer.render_token("▁@#%,;'.-_漢")
-img.show()
+    img = renderer.render_token("▁@#%,;'.-_漢")
+    img.show()
 
 
 
